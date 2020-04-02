9b8c45ac49890a3dc73da8744297b3e4f489eaf6
peterglezroa <peterglezroa@gmail.com>
2020-04-01 19:20:22 -0600

    Diagrams and Problem 67  On branch feature/tree  Changes to be committed: 	new file:   P09_diagram.jpg 	new file:   P67_diagram.jpg 	new file:   p67.jar 	new file:   p67.png 	modified:   problem09.kt 	new file:   problem67.kt

--- problem09.kt
+++ problem09.kt
@@ -2,6 +2,7 @@
   __AUTHOR__: Pedro Luis González Roa
   __STUDENT ID__: A01651517
   __PROBLEM #__: 09
+  __DIAGRAM__ : https://drive.google.com/file/d/1Pb5SPlQfuGBtulM4p7lCR-S8EeaWffOa/view?usp=sharing
 */
 // Problem 09
 fun main(args: Array<String>){
